fb85a256f9a8719d03d44fbcf64e98d733224d71
hikmeterz <hikmeterzioglu@gmail.com>
2024-11-16 02:38:35 +0300

    some updates

---
+++ src/pages/Home.jsx
@@ -0,0 +1,13 @@
+import UserResults from "../components/users/UserResults"
+function Home(params) {
+    return <>
+        {/* SEARCH COMPONENT*/}
+        
+        
+        <UserResults></UserResults>
+    </>
+
+
+}
+
+export default Home
